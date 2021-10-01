3e01843a0c742ee811eaf17f6526d920a1b5c82e
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-01 10:56:59 +0800

    Add project files.

---
+++ W4D5 Training Ses1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace W4D5_Training_Ses1
+{
+    class SampleClass
+    {
+        public void AsyncDisplay()
+        {
+            
+            Thread t = new Thread(() => { CallThisNext(); });
+            Thread tt = new Thread(() => { CallThisNextAgain(); });
+            t.Start();
+            tt.Start();
+
+            Console.WriteLine("Start Thread");
+        }
+
+        public void CallThisNext()
+        {
+            Console.WriteLine("This has no order");
+        }
+
+        public void CallThisNextAgain()
+        {
+            Console.WriteLine("This ALSO has no order");
+        }
+    }
+
+    
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            SampleClass t = new SampleClass();
+            Console.WriteLine("This is called first");
+            
+            Console.WriteLine("Calling this thread Display next");
+            t.AsyncDisplay();
+            Console.ReadLine();
+        }
+    }
+}
